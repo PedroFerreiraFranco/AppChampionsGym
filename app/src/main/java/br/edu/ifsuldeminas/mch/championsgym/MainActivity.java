7234160b87fccdd112443e37abfe963a8c676f89
Pedro Ferreira Franco <pedroferreirafranco22@gmail.com>
2024-08-21 20:00:23 -0300

    Novas atualizações

--- app/src/main/java/br/edu/ifsuldeminas/mch/championsgym/MainActivity.java
+++ app/src/main/java/br/edu/ifsuldeminas/mch/championsgym/MainActivity.java
@@ -1,12 +1,14 @@
 package br.edu.ifsuldeminas.mch.championsgym;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.appcompat.widget.AppCompatButton;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,5 +17,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+
+        AppCompatButton btTreinos = findViewById(R.id.bt_treinos);
+        AppCompatButton btObjetivos = findViewById(R.id.bt_objetivos);
+        AppCompatButton btPerfil = findViewById(R.id.bt_perfil);
+
+        btTreinos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, FormTreino.class);
+                startActivity(intent);
+            }
+        });
+
+        btObjetivos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, FormObjetivo.class);
+                startActivity(intent);
+            }
+        });
+
+        btPerfil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Perfil.class);
+                startActivity(intent);
+            }
+        });
     }
 }
